528df8fa6521c6b716cd907ef007243a2823310d
Patrick Gross <bears_lair@protonmail.com>
2025-04-14 16:49:13 -0700

    basic response from server console logged

--- src/index.js
+++ src/index.js
@@ -1,11 +1,28 @@
 import "./styles.css";
 
-async function currentWeather() {
+const bodyHtml = document.querySelector("body");
+
+const userInputDiv = document.createElement("div");
+bodyHtml.appendChild(userInputDiv);
+
+const locationInput = document.createElement("input");
+userInputDiv.appendChild(locationInput);
+locationInput.setAttribute("placeholder", "Enter a city...");
+
+const submitBtn = document.createElement("button");
+userInputDiv.appendChild(submitBtn);
+submitBtn.textContent = "Submit Location";
+
+submitBtn.addEventListener("click", () => {
+  const location = locationInput.value;
+
+  currentWeather(location);
+});
+
+async function currentWeather(location) {
   fetch(
-    "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/Sacramento?key=4744HCA3XPQ4DV5MVBSTNW4UP"
+    `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?key=4744HCA3XPQ4DV5MVBSTNW4UP`
   ).then(function (response) {
     console.log(response.json());
   });
 }
-
-currentWeather();
